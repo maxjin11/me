d8b37833f7ac89f11dd958ddfe584c269c499e0a
Max Jin <maxjin6011@gmail.com>
2026-08-24 01:05:32 -0400

    Changed resume link

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -84,7 +84,7 @@ export default function Nav() {
           ))}
           <li>
             <a 
-                href="https://maxjin11.github.io/my-resume/Max_Jin___Software_3B_Fall_2026.pdf"
+                href="https://maxjin11.github.io/my-resume/Max_Jin_Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="font-body text-sm font-light tracking-widest text-black hover:text-ember transition-colors"
